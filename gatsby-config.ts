9cdf1c7be391b1a0115d6c16a1af9847e6a7eb63
Johann Schumacher <js.johann.schumacher@gmail.com>
2025-10-27 17:36:26 +0100

    wip

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Psychologische Beratung Olbrich`,
-    siteUrl: `https://jsjohann.github.io/`,
+    siteUrl: `https://jsjohann.github.io/psychologische-beratung-olbrich`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
