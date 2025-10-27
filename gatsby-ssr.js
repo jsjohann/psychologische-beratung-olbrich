682c002b668780248a6d7199b384a5f661d439b7
Johann Schumacher <js.johann.schumacher@gmail.com>
2025-10-27 17:04:05 +0100

    wip

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <link rel="preload" href={"/fonts/Cabin-VariableFont_wdth,wght.ttf"} as="font" type="font/ttf" crossOrigin="anonymous" key="cabin" />
+    <link rel="preload" href={"/psychologische-beratung-olbrich/fonts/Cabin-VariableFont_wdth,wght.ttf"} as="font" type="font/ttf" crossOrigin="anonymous" key="cabin" />
   ]);
 };
