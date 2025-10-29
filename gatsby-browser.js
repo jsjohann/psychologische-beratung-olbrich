d52082032b0b824f0f78220f343cc15af91b86fb
Johann Schumacher <js.johann.schumacher@gmail.com>
2025-10-29 11:32:24 +0100

    fix icons

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
+
 import "./src/styles/global.css"
 
 // Reset scroll position to top when navigating to a new page
